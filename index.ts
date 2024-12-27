54544a67b00b2e7f6f5603b033c26427abb63cfe
Nipun-Mobcoder <nipun.bhardwaj@mobcoder.com>
2024-12-27 18:09:33 +0530

    added func to upload on s3

--- index.ts
+++ index.ts
@@ -4,8 +4,9 @@ import multer from 'multer';
 import dotenv from 'dotenv';
 import { v4 as uuidv4 } from 'uuid';
 import path from 'path';
-import fs from 'fs';
+import fs, { ReadStream } from 'fs';
 import { exec } from 'child_process';
+import { getPresignedUrl, uploadToS3 } from './utils/s3.js';
 
 const app = express();
 dotenv.config();
@@ -26,7 +27,7 @@ const storage = multer.diskStorage({
     }
   })
   
-const upload = multer({storage: storage})
+const upload = multer({storage})
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*") 
@@ -64,22 +65,45 @@ app.post("/upload", upload.single('file'), (req, res) => {
     
         exec(ffmpegCommand, (error, stdout, stderr) => {
             if (error) {
-            console.log(`exec error: ${error}`)
+               console.log(`exec error: ${error}`)
             }
             console.log(`stdout: ${stdout}`)
             console.log(`stderr: ${stderr}`)
             const videoUrl = `http://localhost:5000/uploads/courses/${lessonId}/index.m3u8`;
     
             res.json({
-            message: "Video converted to HLS format",
-            videoUrl: videoUrl,
-            lessonId: lessonId
+                message: "Video converted to HLS format",
+                videoUrl,
+                lessonId
             })
         })
     } catch (e) {
         console.log(e);
         res.status(400).json(e)
     }
+})
+
+app.post("/upload-s3", upload.single('file'), async (req, res) => {
+    try {
+        if(!req.file) 
+            res.status(400).json({ message: "No file uploaded" });
+        else {
+            const { path, filename, mimetype } = req.file;
+            if (!path || !filename || !mimetype)
+                res.status(400).json({ message: "Invalid file data" });
+            const uploadResult = await uploadToS3(path, filename, mimetype);
+         
+            const presignedUrl = await getPresignedUrl(uploadResult.Key);
+        
+            res.json({
+                message: "Video uploaded successfully.",
+                url: presignedUrl
+            })
+            }
+        } catch (error) {
+            console.error("Error during file upload:", error);
+            res.status(500).json({ message: "Error uploading file" });
+        }
 })
 
 app.listen(5000, () => {
---
+++ utils/s3.ts
@@ -0,0 +1,31 @@
+import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
+import { readFileSync } from 'fs';
+import { v4 as uuidv4 } from 'uuid';
+
+dotenv.config();
+
+export const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+export const uploadToS3 = (path: string, filename: string, mimetype: string) => {
+  const s3Params = {
+    Bucket: process.env.S3_BUCKET_NAME || '',
+    Key: `${uuidv4()}-${filename}`,
+    Body: readFileSync(path),
+    ContentType: mimetype,
+  };
+  return s3.upload(s3Params).promise();
+};
+
+export const getPresignedUrl = (key: string) => {
+  const s3Params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: key,
+    Expires: 60 * 60,
+  };
+  return s3.getSignedUrlPromise('getObject', s3Params);
+};
